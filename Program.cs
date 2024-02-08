5c3f57421f57300bb590587cfadd8097be4f3179
mperaltarj <mperaltarj@gmail.com>
2024-02-08 17:41:15 -0300

    Entrega do Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+
+
+Console.WriteLine("Smartfone Nokia: ");
+Nokia n = new Nokia("(21)98225-6936", "Nokia Tijolo", "MAR8745", 32);
+n.Ligar();
+n.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartfone Iphone: ");
+Iphone i = new Iphone("(21)98225-6930", "Iphone 12", "MAR8743", 64);
+i.ReceberLigacao();
+i.InstalarAplicativo("Telegram");
